5d7a5f63d6b53bd116316d6198ae74a72b3d40cb
root <root@ip-172-31-28-68.us-west-2.compute.internal>
2018-04-22 05:45:12 +0000

    Updated permissions

--- salt_install.sh
+++ salt_install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 yum install -y https://repo.saltstack.com/yum/redhat/salt-repo-latest-2.el7.noarch.rpm
-yum clean expire-cache\
+yum clean expire-cache
 yum install -y salt salt-minion
